6c809930ece148cd0a56ac38efe2dde264b961bd
suwedaa <suweda.afrah@gmail.com>
2024-10-15 09:35:42 -0400

    Hello World

---
+++ src/main/java/com/bptn/Vehicle_Rental_System/Hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.bptn.Vehicle_Rental_System.Hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
